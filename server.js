b91f1e2f6f881aacdc4e0fa79e08f77ab155ecba
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-08-13 10:13:02 -0400

    Add DB Config to server.js

--- server.js
+++ server.js
@@ -8,4 +8,14 @@ const app = express();
 app.use(bodyParser.json());
 
 
+// DB Config
+const db = require('./config/keys').mongoURI;
 
+// Connect to MongoDB
+mongoose.connect(db)
+.then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
+
+// Designate port
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
